4b36cf2dfaa0d09c567aaab8cfb99156c304e20c
mikelakala <makala.michael@yahoo.com>
2016-10-24 01:58:11 -0300

    creating test model

---
+++ src/main/java/com/dome/base/model/Test.java
@@ -0,0 +1,25 @@
+package com.dome.base.model;
+
+/**
+ * Created by akalamichael on 10/24/16.
+ */
+public class Test {
+    private int id;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
