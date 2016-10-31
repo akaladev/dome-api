ecfe79b3cf33bd6f53c6851a29ecf9d3298c74d4
mikelakala <makala.michael@yahoo.com>
2016-10-31 12:59:38 -0300

    exposing all service endpoints and backend api

---
+++ src/main/java/com/dome/base/application/SpringBootWebApplication.java
@@ -0,0 +1,48 @@
+package com.dome.base.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.PathSelectors;
+
+@SpringBootApplication
+@EnableSwagger2
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    @Bean
+    public Docket petApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .contact("Niklas Heidloff")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
+                .build();
+    }
+
+}
---
+++ src/main/java/com/dome/base/endpoints/ColorController.java
@@ -0,0 +1,71 @@
+package com.dome.base.endpoints;
+
+import java.util.List;
+
+import com.dome.base.bindings.ColorDao;
+import com.dome.base.application.Application;
+import com.dome.base.application.exception.ComponentNotFoundException;
+import com.dome.base.model.Color;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@Api(basePath = "/color", description = "Create and Get Colors Added", produces = "application/json")
+
+@RestController
+public class ColorController {
+    
+    
+    @ApiOperation(value = "Find all stored colors")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "") })
+    @RequestMapping(value = "/color/findByList", method = GET)
+    public ResponseEntity<List<Color>>  getColor(){
+     try {
+          Application application = new Application();
+          ColorDao dao = (ColorDao) application.getComponent("colorDaoImpl");
+          List<Color> colors = dao.findByList();
+          return new ResponseEntity<List<Color>>(colors, OK);
+     }catch(ComponentNotFoundException exp){
+            exp.printStackTrace();
+        }
+        return null;
+    }
+    
+    @ApiOperation(value = "add a color with a given name.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "")
+    })
+    @RequestMapping(value = "/color/", method = POST)
+    public ResponseEntity<Void> createColor(@RequestBody Color color, UriComponentsBuilder ucBuilder) {
+        try {
+          Application application = new Application();
+          ColorDao dao = (ColorDao) application.getComponent("colorDaoImpl");
+          dao.save(color);
+          HttpHeaders headers = new HttpHeaders();
+     }catch(ComponentNotFoundException exp){
+            exp.printStackTrace();
+        }
+        return null;        
+    }
+    
+}
+
+
+ 
