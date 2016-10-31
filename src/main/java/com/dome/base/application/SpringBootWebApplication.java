904544e5923a4da9d1d3d80a59a71fc90f92a37d
mikelakala <makala.michael@yahoo.com>
2016-10-31 16:17:48 -0300

    exposing all service endpoints and backend api

--- src/main/java/com/dome/base/application/SpringBootWebApplication.java
+++ src/main/java/com/dome/base/application/SpringBootWebApplication.java
@@ -11,37 +11,42 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import com.google.common.base.Predicates;
 import springfox.documentation.builders.PathSelectors;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableSwagger2
+@ComponentScan("com.dome.base.service")
 public class SpringBootWebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
 
-    @Bean
-    public Docket petApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(SpringBootWebApplication.class);
     }
+    
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/error"))) // Exclude Spring error controllers
+                .build();
+    }
+
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring REST Sample with Swagger")
-                .description("Spring REST Sample with Swagger")
-                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
-                .contact("Niklas Heidloff")
+                .title("Dome API")
+                .description("An API that exposes backend data ")
+                .termsOfServiceUrl("")
+                .contact("Akala Michael")
                 .license("Apache License Version 2.0")
-                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
-                .version("2.0")
+                .licenseUrl("")
+                .version("1.0")
                 .build();
     }
 
