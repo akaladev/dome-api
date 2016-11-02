97a29b28fef3b887067303ec166a514387a313c2
mikelakala <makala.michael@yahoo.com>
2016-11-02 01:33:21 -0300

    pushing executable jar.

--- src/main/java/com/dome/base/utils/TestUtils.java
+++ src/main/java/com/dome/base/utils/TestUtils.java
@@ -5,18 +5,6 @@ import com.dome.base.application.Application;
 import com.dome.base.application.exception.ComponentNotFoundException;
 
 public class TestUtils {
-    public static void main(String args[]){
-        Application application = new Application();
-        try {
 
-            ColorDao dao = (ColorDao) application.getComponent("colorDaoImpl");
-            Color color = new Color();
-            color.setName("blue");
-           // dao.save(color);
 
-           // System.out.println(dao.findByList().get(0).getName());
-        }catch(ComponentNotFoundException exp){
-            exp.printStackTrace();
-        }
-    }
 }
